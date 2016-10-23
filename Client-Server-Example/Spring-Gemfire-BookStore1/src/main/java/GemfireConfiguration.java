4058379b05d3986e3f3cf25dbda310c1765c9460
Barath Arivazhagan <barathmacdec91@gmail.com>
2016-10-23 19:55:02 +0530

    Update GemfireConfiguration.java

--- Client-Server-Example/Spring-Gemfire-BookStore1/src/main/java/GemfireConfiguration.java
+++ Client-Server-Example/Spring-Gemfire-BookStore1/src/main/java/GemfireConfiguration.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.gemfire.ReplicatedRegionFactoryBean;
 import org.springframework.data.gemfire.client.ClientRegionFactoryBean;
-
+import org.springframework.beans.factory.annotation.Value;
 import com.gemstone.gemfire.cache.Region;
 import com.gemstone.gemfire.cache.client.ClientCache;
 import com.gemstone.gemfire.cache.client.ClientCacheFactory;
@@ -16,32 +16,26 @@ public class GemfireConfiguration {
 	
 	
 	private static final String REGION_NAME="BOOK";
-	private static final String HOST="192.168.1.39";
-	private static final int PORT=10334;
+	
+	
+	
+	@Value("${gemfire.locator.host:localhost}")
+	private String gemfireHost;
+	
+	@Value("${gemfire.locator.port:10334}")
+	private int gemfirePort;
 	
 	
 	@Bean
 	public ClientCache clientCache(){
 		
-		ClientCache c = new ClientCacheFactory().addPoolLocator(HOST, PORT).create();
+		ClientCache clientCache = new ClientCacheFactory().addPoolLocator(gemfireHost, gemfirePort).create();
 		
-		 Region<Object, Object> region = c.createClientRegionFactory(ClientRegionShortcut.PROXY).create("BOOK");
-		//System.out.println("Region ===> "+r);
-		return c;
+		 Region<Object, Object> region = clientCache.createClientRegionFactory(ClientRegionShortcut.PROXY).create("BOOK");
+		
+		return clientCache;
 	}
 	
-//	@Bean
-//	public ClientRegionFactoryBean<Long, Book> bookRegion(){
-//		ClientRegionFactoryBean<Long, Book> bookRegion=new ClientRegionFactoryBean<Long, Book>();
-//		bookRegion.setCache(clientCache());
-//		bookRegion.setShortcut(ClientRegionShortcut.PROXY);
-//		return bookRegion;
-//	}
-	
-//	@Bean
-//	public ReplicatedRegionFactoryBean<Long, Book> bookRegion(){
-//		ReplicatedRegionFactoryBean<Long, Book> bookRegion=new ReplicatedRegionFactoryBean<Long, Book>();
-//		return bookRegion;
-//	}
+
 
 }
